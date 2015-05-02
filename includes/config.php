f148df6698c4c55259105ffb0f5697296f590df8
Veselin Penev <veselinpenev@abv.bg>
2015-05-02 13:58:02 +0300

    Fix bug

--- includes/config.php
+++ includes/config.php
@@ -3,12 +3,8 @@ define('DEFAULT_CONTROLLER', 'home');
 define('DEFAULT_ACTION', 'index');
 define('DEFAULT_LAYOUT', 'default');
 
-// define('DB_HOST', 'localhost');
-// define('DB_USER', 'root');
-// define('DB_PASS', '');
-// define('DB_NAME', 'forum');
-
-define('DB_HOST', '127.9.203.2:3306');
-define('DB_USER', 'admin2STDV3B');
-define('DB_PASS', 'YYceuWjrsneN');
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
 define('DB_NAME', 'forum');
+
