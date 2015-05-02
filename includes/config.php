59a174f73d05c6e53dfdc442d4bc283100154225
Veselin Penev <veselinpenev@abv.bg>
2015-05-02 14:26:05 +0300

    change database connection

--- includes/config.php
+++ includes/config.php
@@ -3,8 +3,12 @@ define('DEFAULT_CONTROLLER', 'home');
 define('DEFAULT_ACTION', 'index');
 define('DEFAULT_LAYOUT', 'default');
 
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_HOST', '127.12.33.130:3306');
+define('DB_USER', 'adminQIiJNx1');
+define('DB_PASS', 'qji85cgEEGPv');
 define('DB_NAME', 'forum');
 
+//define('DB_HOST', 'localhost');
+//define('DB_USER', 'root');
+//define('DB_PASS', '');
+//define('DB_NAME', 'forum');
