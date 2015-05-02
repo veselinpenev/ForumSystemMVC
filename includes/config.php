01f65af659f80932eb243bb26567ef1e5a6a856c
Veselin Penev <veselinpenev@abv.bg>
2015-05-02 14:34:51 +0300

    change mysql connection

--- includes/config.php
+++ includes/config.php
@@ -3,7 +3,7 @@ define('DEFAULT_CONTROLLER', 'home');
 define('DEFAULT_ACTION', 'index');
 define('DEFAULT_LAYOUT', 'default');
 
-define('DB_HOST', '127.12.33.130:3306');
+define('DB_HOST', 'mysql://$OPENSHIFT_MYSQL_DB_HOST:$OPENSHIFT_MYSQL_DB_PORT/');
 define('DB_USER', 'adminQIiJNx1');
 define('DB_PASS', 'qji85cgEEGPv');
 define('DB_NAME', 'forum');
