9bc527f3cf418fdd78b9a8ca4070efbb21f0bae8
Veselin Penev <veselinpenev@abv.bg>
2015-05-02 15:12:15 +0300

    change db connection

--- includes/config.php
+++ includes/config.php
@@ -3,7 +3,7 @@ define('DEFAULT_CONTROLLER', 'home');
 define('DEFAULT_ACTION', 'index');
 define('DEFAULT_LAYOUT', 'default');
 
-define('DB_HOST', 'https://php-forumsystem.rhcloud.com:443');
+define('DB_HOST', '127.12.33.130');
 define('DB_USER', 'adminQIiJNx1');
 define('DB_PASS', 'qji85cgEEGPv');
 define('DB_NAME', 'forum');
